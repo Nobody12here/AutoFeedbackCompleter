b5827352e7477f284c8ad344acdcd7fd18e8ab62
Nobody12here <78294230+Nobody12here@users.noreply.github.com>
2022-01-03 18:29:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 clear()
-var RandomQuotes=["Do not take life too seriously. You will never get out of it alive.",
-"A day without sunshine is like, you know, night.",
-"Laziness is nothing more than the habit of resting before you get tired.",
-"My life needs editing.",
-"If you don't mind, it doesn't matter.",
-"A successful man is one who makes more money than his wife can spend. A successful woman is one who can find such a man."]
-var randomNumber = Math.floor(Math.random()*6)
 var textArea = document.querySelectorAll("textarea.form-control")
 for (var i = 0;i<textArea.length; i++) {
   textArea[i].value = RandomQuotes[randomNumber]
@@ -13,6 +6,5 @@ for (var i = 0;i<textArea.length; i++) {
 var Radiobtns = document.querySelectorAll('input[type="radio"]')
 console.log("Btn = ",Radiobtns.length);
 for(var i=0;i<Radiobtns.length;i++){
-  
   Radiobtns[i].checked = true;
  }
